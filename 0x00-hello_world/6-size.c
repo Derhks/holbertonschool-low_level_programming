0159e44b2f227be8e46a6e54dbd1d7c8de9d9106
Derhks <derhks@gmail.com>
2019-09-19 01:57:27 +0000

    How to know the data of each type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Main function
+ *printf - write output to stdout
+ *Description: Display a message
+ *section header: Section description
+ *Return: ends the code block
+ */
+int main(void)
+{
+
+	printf("Size of a char: %lu.\n", sizeof(char));
+	printf("Size of an int: %lu.\n", sizeof(int));
+	printf("Size of a long int: %lu.\n", sizeof(long int));
+	printf("Size of a long long int: %lu.\n", sizeof(long long int));
+	printf("Size of a float: %lu.\n", sizeof(float);
+	return (0);
+}
