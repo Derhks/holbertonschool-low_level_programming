e93f204a18327800a0515388d0fa60b1b5a682eb
Derhks <derhks@gmail.com>
2019-09-19 00:40:46 +0000

    How to print a message using 'printf'

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Main function
+ *printf - write output to stdout
+ *Description: Display a message
+ *section header: Section description
+ *Return: ends the code block
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	retunr(0);
+}
