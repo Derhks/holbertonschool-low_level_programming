3b7094ec6dd12904792a0c12813c810a69788a11
Derhks <derhks@gmail.com>
2019-09-19 22:00:01 +0000

    How to display the last digit of a number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *main - Main function
+ *printf - write output to stdout
+ *n - keeps a random number
+ *last_digit - saves the random number of n
+ *Description: Display a message
+ *section header: Section description
+ *Return: ends the code block
+ */
+
+int main(void)
+{
+	int n;
+	int lst_dgt;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	lst_dgt = n % 10;
+
+	if (lst_dgt > 5)
+	{
+		printf("Last digit of %i is %d and is greater than 5\n", n, lst_dgt);
+	}
+	else if (lst_dgt == 0)
+	{
+		printf("Last digit of %i is %d and is and is 0\n", n, lst_dgt);
+	}
+	else
+	{
+		printf("Last digit of %i is %d and is less than 6 and not 0\n", n, lst_dgt);
+	}
+	return (0);
+}
