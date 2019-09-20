d2ed6a750400633ad23d955bd175967abfe56837
Derhks <derhks@gmail.com>
2019-09-20 20:35:47 +0000

    how to display a random number and tell if it is positive or negative

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,9 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
  *main - Main function
  *printf - write output to stdout
@@ -29,7 +27,7 @@ int main(void)
 	}
 	else
 	{
-		     printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
