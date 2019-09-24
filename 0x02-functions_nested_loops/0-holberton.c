99f808b2cc2fdd381f102ee7b044cb36a72ef6ae
Derhks <derhks@gmail.com>
2019-09-24 15:43:45 +0000

    How to display Holberton using '_putchar'

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *main - Main function
+ *_putchar - write a character, of unsigned char type, to stdout
+ *Description: Display Holberton
+ *section header: Section description
+ *Return: ends the code block
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
