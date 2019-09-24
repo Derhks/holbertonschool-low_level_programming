d8e9ddbe396ffad19d9788a87d60c255f6c87dd1
Derhks <derhks@gmail.com>
2019-09-24 16:40:42 +0000

    How to display the alphabet using a function prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *main - Main function
+ *_putchar - write a character, of unsigned char type, to stdout
+ *Description: Display Holberton
+ *section header: Section description
+ *Return: ends the code block
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for ( alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
