d79c7758ad98dc0343759b458277d027ca9bbf55
Derhks <derhks@gmail.com>
2019-09-25 21:53:29 +0000

    How to display the sum of two integers using function prototype

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *add - This is a function prototype
+ *@x: It the first integer
+ *@y: It the second integer
+ *Description: Display the result of the add the two integers
+ *section header: Section description
+ *Return: end of the function main
+ */
+
+int add(int x, int y)
+{
+	int result = x + y;
+
+	return (result);
+}
