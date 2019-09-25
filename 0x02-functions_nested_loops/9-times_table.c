a5d5db5606ddd21a5cd7b611db1afd80050116aa
Derhks <derhks@gmail.com>
2019-09-25 18:30:25 +0000

    How to display the 9 times table, starting with zero

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ *times_tables - This is a function prototype
+ *Description: Display the 9 times table
+ *section header: Section description
+ *Return: end of the function main
+ */
+
+void times_table(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			int z = x * y;
+
+			if (z > 0 && z <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else if (z >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
