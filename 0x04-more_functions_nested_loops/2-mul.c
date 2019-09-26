d6291f105951ec5fd2e20344de814cd101e3e7e7
Derhks <derhks@gmail.com>
2019-09-26 17:06:25 +0000

    How to multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *mul - This is a function prototype
+ *@a: This variable recive the first value of the function main
+ *@b: This variable recive the second value of the function main
+ *Description: This function multiplies two integers
+ *section header: Section description
+ *Return: mult, multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	int mult = a * b;
+
+	return (mult);
+}
