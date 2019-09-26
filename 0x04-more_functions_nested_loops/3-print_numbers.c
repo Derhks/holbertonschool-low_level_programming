523b7da3fc59539b83958fcf7a05d4f294c97304
Derhks <derhks@gmail.com>
2019-09-26 17:07:46 +0000

    How to prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - This is a function prototype
+ *Description: Display 1 if the letter is lowercase and 0 if don't
+ *section header: Section description
+ *Return: 1 if the letter is lowercase and 0 if don't
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
