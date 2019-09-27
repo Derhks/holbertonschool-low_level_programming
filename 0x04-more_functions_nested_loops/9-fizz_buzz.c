80b3e32f027a3639cae0961598fb899c6ef74679
Derhks <derhks@gmail.com>
2019-09-27 02:11:46 +0000

    How to prints the numbers from 1 to 100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ *main - This is the main function
+ *Description: Prints the numbers from 1 to 100
+ *section header: Section description
+ *Return: End of the block of code
+ */
+
+int main(void)
+{
+	int tst;
+
+	for (tst = 1; tst <= 100; tst++)
+	{
+		if (((tst % 3) == 0) && ((tst % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((tst % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((tst % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", tst);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
