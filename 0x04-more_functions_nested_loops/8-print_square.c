2da383a6e7e00dd62a8d07dff09d79408fd1b295
Derhks <derhks@gmail.com>
2019-09-27 00:16:52 +0000

    How to prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *print_square - This is a function prototype
+ *@size: This variable recive the value of the function main
+ *Description: Prints a square
+ *section header: Section description
+ */
+
+void print_square(int size)
+{
+	int square = size;
+	int sqr1;
+	int sqr2;
+
+	for (sqr1 = 0; sqr1 < square; sqr1++)
+	{
+		for(sqr2 = 0; sqr2 < square; sqr2++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
