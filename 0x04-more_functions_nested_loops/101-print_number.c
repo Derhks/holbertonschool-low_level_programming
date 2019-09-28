217e0e1ae6b4c52a74639bc42a20003585d58e58
Derhks <derhks@gmail.com>
2019-09-28 23:13:22 +0000

    How to prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+ *print_number - This is a function prototype
+ *@n: This variable recive the value of the function main
+ *Description: Prints a square
+ *section header: Section description
+ */
+
+void print_number(int n)
+{
+	int y;
+
+	y = ((n % 1000) % 100);
+
+	if (n < 0 && n > -100)
+	{
+		_putchar('-');
+		_putchar(-n / 10 + '0');
+		_putchar(-n % 10 + '0');
+	}
+	else if (n == 0)
+	{
+		_putchar(n + '0');
+	}
+	else if (n > 0 && n < 100)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 100 && n < 1000)
+	{
+		_putchar(n / 100 + '0');
+		_putchar(((n % 100) / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n >= 1000 && n < 10000)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(n = ((n % 1000) / 100) + '0');
+		_putchar((y / 10) + '0');
+		_putchar((y % 10) + '0');
+	}
+}
