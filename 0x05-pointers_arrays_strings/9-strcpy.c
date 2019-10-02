a3cde4cae8193a09357b7c2d0e94cbd4e458bba6
Derhks <derhks@gmail.com>
2019-10-02 03:29:09 +0000

    How to copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - This is a function prototype
+ * @dest: This variable recive the value of the function main
+ * @src: This variable recive the value of the function main
+ * Description: Function that copies the string pointed to by src
+ * section header: Section description
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count1 = 0;
+	int count2;
+
+	while (src[count1] != '\0')
+	{
+		count1++;
+	}
+	for (count2 = 0; count2 <= count1; count2++)
+	{
+		dest[count2] = src[count2];
+	}
+	return (dest);
+}
