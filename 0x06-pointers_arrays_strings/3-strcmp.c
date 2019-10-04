cc7ed50735c30fce5ca9d8961f38055c8a9003cd
Derhks <derhks@gmail.com>
2019-10-04 00:14:36 +0000

    How to compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,19 +12,19 @@
 int _strcmp(char *s1, char *s2)
 {
 	int count1;
-	int numb1 = 0;
-	int numb2 = 0;
-	int numb3;
+	int numb1;
 
 	for (count1 = 0; s1[count1] != '\0'; count1++)
 	{
 		if (s2[count1] != '\0' && (s1[count1] != s2[count1]))
 		{
-		numb1 = s1[count1];
-		numb2 = s2[count1];
+			numb1 = s1[count1] - s2[count1];
 		}
-		numb3 = numb1 - numb2;
+		else
+		{
+			return (0);
+		}
 		break;
 	}
-	return (numb3);
+	return (numb1);
 }
