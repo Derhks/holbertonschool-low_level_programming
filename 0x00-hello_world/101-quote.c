7c8180ffea37f929c1be396612c0d86533dcc2eb
Derhks <derhks@gmail.com>
2019-10-06 20:37:19 +0000

    How to print a string using the function write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * *main - This is a main function
@@ -9,13 +9,7 @@
 
 int main(void)
 {
-	char charts[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	int count1;
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	for (count1 = 0; charts[count1] != '\0'; count1++)
-	{
-		putchar(charts[count1]);
-	}
-	putchar('\n');
 	return (1);
 }
