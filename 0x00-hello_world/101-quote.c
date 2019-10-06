c715a4163f740d263d093bbc6d2d791ff8bef858
Derhks <derhks@gmail.com>
2019-10-06 18:14:28 +0000

    How to prints a string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 
 /**
  * *main - This is a main function
- * Description: Prints exactly "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * Description: and that piece of art is useful" - Dora Korpar, 2015-10-19
  * section header: Section description
  * Return: Returns always 1
  */
 
-int main (void)
+int main(void)
 {
 	char charts[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int count1;
