27cc4a463bef3dc5e51c3546e19088a126185e5e
Derhks <derhks@gmail.com>
2019-10-08 01:42:54 +0000

    How to searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk - This is a function prototype
+ * @s: This variable recive the first value of the function main
+ * @accept: This variable recive the second value of the function main
+ * Description: Function that searches a string for any of a set of bytes
+ * section header: Section description
+ * Return: Returns the result of the prototype function to the main function
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+}
