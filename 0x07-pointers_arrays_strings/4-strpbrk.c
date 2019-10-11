c5600dd454978d23945d25fc3bde628b3e6f96ee
Derhks <derhks@gmail.com>
2019-10-11 21:00:41 +0000

    How to searche a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,9 +6,27 @@
  * @accept: This variable recive the second value of the function main
  * Description: Function that searches a string for any of a set of bytes
  * section header: Section description
- * Return: Returns the result of the prototype function to the main function
+ * Return: The result of the prototype function to the main function
  */
 
 char *_strpbrk(char *s, char *accept)
 {
+	int count1;
+	int count2;
+
+	for (count1 = 0; s[count1] != '\0'; count1++)
+	{
+		for (count2 = 0; accept[count2] != '\0'; count2++)
+		{
+			if (s[count1] == accept[count2])
+			{
+				return (s + count1);
+			}
+			if (s[count1] != accept[count2] && s[count1 + 1] == '\0')
+			{
+				return ('\0');
+			}
+		}
+	}
+	return (0);
 }
