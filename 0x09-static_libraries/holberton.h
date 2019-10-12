a97aee87194bb9e3272add9a2be2dc9cc8c46cfd
Derhks <derhks@gmail.com>
2019-10-12 22:20:51 +0000

    This is my header file

---
+++ 0x09-static_libraries/holberton.h
@@ -0,0 +1,25 @@
+#ifndef MY_OWN_HEADER_FILE
+#define MY_OWN_HEADER_FILE
+
+int _isupper(int c);
+char *_memset(char *s, char b, unsigned int n);
+char *_strcat(char *dest, char *src);
+int _atoi(char *s);
+int _isdigit(int c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strchr(char *s, char c);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+int _islower(int c);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+unsigned int _strspn(char *s, char *accept);
+int _isalpha(int c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *f, char *s);
+int _abs(int r);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+
+#endif
