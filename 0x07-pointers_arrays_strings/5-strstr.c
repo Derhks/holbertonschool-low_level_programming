3f84ed35ca92994c10251af50eba6b684a107344
Derhks <derhks@gmail.com>
2019-10-12 21:31:22 +0000

    How to locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,27 +2,34 @@
 
 /**
  * *_strstr - This is a function prototype
- * @haystack: This variable recive the first value of the function main
- * @needle: This variable recive the second value of the function main
+ * @f: This variable recive the first value of the function main
+ * @s: This variable recive the second value of the function main
  * Description: Function that locates a substring
  * section header: Section description
  * Return: The result of the prototype function to the main function
  */
 
-char *_strstr(char *haystack, char *needle)
+char *_strstr(char *f, char *s)
 {
-	int count1;
-	int count2;
+	int cnt1;
+	int cnt2;
+	int cnt3;
+	int pt = 0;
 
-	for (count1 = 0; haystack[count1] != '\0'; count1++)
+	for (cnt1 = 0; s[cnt1] != '\0'; cnt1++)
 	{
-		for (count2 = 0; needle[count2] != '\0'; count2++)
+	}
+	for (cnt2 = 0; f[cnt2] != '\0'; cnt2++)
+	{
+		for (cnt3 = 0; s[cnt3] != '\0'; cnt3++)
 		{
-			if (haystack[count1] == needle[count2])
+			if (f[cnt2] == s[cnt3] && f[cnt2 + 1] == s[cnt3 + 1])
 			{
-				if (haystack[count1 + 1] == needle[count2 + 1])
+				pt = cnt2;
+				cnt2++;
+				if (f[pt] == s[0] && f[pt + (cnt1 - 1)] == s[cnt1 - 1])
 				{
-					return (haystack + count1);
+					return (f + pt);
 				}
 			}
 		}
