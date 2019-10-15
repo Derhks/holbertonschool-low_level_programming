7d8d5103dc4c7db24bfe9ecc3fc1885ea166bffd
Derhks <derhks@gmail.com>
2019-10-15 22:42:18 +0000

    How to returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - This is a function prototype
+ * @str: This variable recive the first value of the function main
+ * Description: Returns a pointer to a newly allocated space in memory
+ * section header: Section description
+ * Return: Return a pointer
+ */
+
+char *_strdup(char *str)
+{
+	int cnt1;
+	char *strng;
+
+	for (cnt1 = 0; str[cnt1] != '\0'; cnt1++)
+	{
+	}
+
+	strng = (char *)malloc(sizeof(char) * cnt1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (cnt1 = 0; str[cnt1] != '\0'; cnt1++)
+	{
+		strng[cnt1] = str[cnt1];
+	}
+	return (strng);
+}
