7fd54073df0bb0beca7b7085d6282a82abe2825b
Derhks <derhks@gmail.com>
2019-10-17 19:52:00 +0000

    How to concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * *str_concat - This is a function prototype
+ * *string_nconcat - This is a function prototype
  * @s1: This variable recive the first value of the function main
  * @s2: This variable recive the second value of the function main
  * @n: This variable recive the third value of the function main
@@ -13,42 +13,44 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int cnt1;
-	unsigned int cnt2;
-	char *strcat;
+	unsigned int cnt1, cnt2;
+	char *strncat;
 
 	for (cnt1 = 0; s1[cnt1] != '\0'; cnt1++)
 	{
 	}
-
 	for (cnt2 = 0; s2[cnt2] != '\0'; cnt2++)
 	{
 	}
-
-	strcat = malloc((sizeof(char) * (cnt1 + n)) + 1);
-
-	if (strcat == NULL)
+	if (n >= cnt2)
+	{
+		strncat = malloc(sizeof(char) * (cnt1 + cnt2) + 1);
+	}
+	else
+	{
+		strncat = malloc(sizeof(char) * (cnt1 + n));
+	}
+	if (strncat == NULL)
 	{
 		return (NULL);
 	}
-
 	for (cnt1 = 0; s1[cnt1] != '\0'; cnt1++)
 	{
-		strcat[cnt1] = s1[cnt1];
+		strncat[cnt1] = s1[cnt1];
 	}
-	if (n > cnt2)
+	if (n >= cnt2)
 	{
 		for (cnt2 = 0; s2[cnt2] != '\0'; cnt2++)
 		{
-			strcat[cnt1 + cnt2] = s2[cnt2];
+			strncat[cnt1 + cnt2] = s2[cnt2];
 		}
 	}
 	if (n < cnt2)
 	{
-		for (cnt2 = 0; cnt2 <= n; cnt2++)
+		for (cnt2 = 0; cnt2 < n; cnt2++)
 		{
-			strcat[cnt1 + cnt2] = s2[cnt2];
+			strncat[cnt1 + cnt2] = s2[cnt2];
 		}
 	}
-	return (strcat);
+	return (strncat);
 }
