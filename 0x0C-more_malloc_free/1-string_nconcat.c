48cb02f03f892e283915196a20043ddec1a5a9cc
Derhks <derhks@gmail.com>
2019-10-17 20:52:16 +0000

    How to concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,12 +19,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	(s1 == NULL) ? s1 = "" : s1;
 	(s2 == NULL) ? s2 = "" : s2;
 
-	for (cnt1 = 0; s1[cnt1] != '\0'; cnt1++)
-	{
-	}
-	for (cnt2 = 0; s2[cnt2] != '\0'; cnt2++)
-	{
-	}
+	cnt1 = _strlen(s1);
+	cnt2 = _strlen(s2);
 
 	if (n >= cnt2)
 	{
@@ -50,4 +46,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	strncat[cnt1 + cnt2] = '\0';
 	return (strncat);
+}
+
+
+/**
+ * _strlen - This is a function prototype
+ * @s: This variable recive the value of the function main
+ * Description: Function that returns the length of a string
+ * section header: Section description
+ * Return: Returns the counter value to the main function
+ */
+
+int _strlen(char *s)
+{
+        int count = 0;
+
+        while (s[count] != '\0')
+        {
+                count++;
+        }
+        return (count);
 }
