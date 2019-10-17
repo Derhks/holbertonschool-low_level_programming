a6c542d5750c821de92095c95a45af9bce3b8399
Derhks <derhks@gmail.com>
2019-10-17 22:11:29 +0000

    How to allocates memory for an array, using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc - This is a function prototype
+ * @nmemb: This variable recive the first value of the function main
+ * @size: This variable recive the second value of the function main
+ * Description: Function that allocates memory for an array
+ * section header: Section description
+ * Return: Return a pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int cnt1;
+	char *array2d;
+
+	if (nmemb == 0)
+	{
+		return (NULL);
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array2d = malloc(sizeof(int *) * nmemb);
+
+	if (array2d == NULL)
+	{
+		return (NULL);
+	}
+
+	for (cnt1 = 0; cnt1 <= size; cnt1++)
+	{
+		array2d[cnt1] = 0;
+	}
+	return (array2d);
+}
