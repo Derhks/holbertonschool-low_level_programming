8948735bf6905c192aae80e4244947af1f2be496
Derhks <derhks@gmail.com>
2019-10-17 20:41:59 +0000

    How to concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,20 +16,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int cnt1, cnt2;
 	char *strncat;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
+	(s1 == NULL) ? s1 = "" : s1;
+	(s2 == NULL) ? s2 = "" : s2;
+
 	for (cnt1 = 0; s1[cnt1] != '\0'; cnt1++)
 	{
 	}
 	for (cnt2 = 0; s2[cnt2] != '\0'; cnt2++)
 	{
 	}
+
 	if (n >= cnt2)
 	{
 		strncat = malloc(sizeof(char) * (cnt1 + cnt2) + 1);
@@ -38,6 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		strncat = malloc(sizeof(char) * (cnt1 + n) + 1);
 	}
+
 	if (strncat == NULL)
 	{
 		return (NULL);
@@ -46,9 +43,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		strncat[cnt1] = s1[cnt1];
 	}
+
 	for (cnt2 = 0; (s2[cnt2] != '\0') && (cnt2 < n); cnt2++)
 	{
 		strncat[cnt1 + cnt2] = s2[cnt2];
 	}
+	strncat[cnt1 + cnt2] = '\0';
 	return (strncat);
 }
