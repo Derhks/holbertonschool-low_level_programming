fe516a3413ff7feec4130dda91da4b9b0eed2711
Derhks <derhks@gmail.com>
2019-11-01 00:59:24 +0000

    [fix] betty, line 37

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -34,6 +34,7 @@ int _strlen(const char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 	{
 		return (NULL);
