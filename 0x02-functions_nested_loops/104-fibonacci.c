83640b9c79c2b7ba53c077d0760ccad799913814
Derhks <derhks@gmail.com>
2019-11-02 20:51:08 +0000

    How to prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,31 +2,32 @@
 
 /**
  * main - This is a main function
- * Description: Prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * Description: Prints the first 98 Fibonacci numbers, starting with 1 and 2
  * section header: Section description
  * Return: Always 0
  */
 
 int main(void)
 {
-	int cnt1, strd1, strd2, fbnc;
+	long int cnt1;
+	unsigned long int strd1, strd2, fbnc;
 
-	for (cnt1 = 1; cnt1 <= 50; cnt1++)
+	for (cnt1 = 1; cnt1 <= 98; cnt1++)
 	{
 		if (cnt1 == 1)
 		{
 			strd1 = 1;
-			printf("%d, ", strd1);
+			printf("%lu, ", strd1);
 		}
 		else if (cnt1 == 2)
 		{
 			strd2 = 2;
-			printf("%d", strd2);
+			printf("%lu", strd2);
 		}
 		else
 		{
 			fbnc = strd1 + strd2;
-			printf(", %d", fbnc);
+			printf(", %lu", fbnc);
 			strd1 = strd2;
 			strd2 = fbnc;
 		}
