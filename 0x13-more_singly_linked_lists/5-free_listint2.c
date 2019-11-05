d47581eab799f849d995c0ba3f7ffc54cdd8f760
Derhks <derhks@gmail.com>
2019-11-05 17:33:53 +0000

    [fix] the variable is initialized after

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,16 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temporal = *head;
+	listint_t *temporal;
 	listint_t *next;
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		return;
 	}
+
+	temporal = *head;
+
 	while (temporal != NULL)
 	{
 		next = temporal->next;
