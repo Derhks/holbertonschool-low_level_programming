873c6f2cb588c382647df21970db11fc69517121
Derhks <derhks@gmail.com>
2019-11-05 17:36:22 +0000

    [fix] all the code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,21 +9,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temporal;
 	listint_t *next;
 
 	if (*head == NULL)
 	{
 		return;
 	}
-
-	temporal = *head;
-
-	while (temporal != NULL)
+	while (*head != NULL)
 	{
-		next = temporal->next;
-		free(temporal);
-		temporal = next;
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
 	*head = NULL;
 }
