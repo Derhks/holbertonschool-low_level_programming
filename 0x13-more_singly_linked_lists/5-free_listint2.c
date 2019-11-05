30bebab4b49763364a4b508074b6361df5150ab3
Derhks <derhks@gmail.com>
2019-11-05 16:22:12 +0000

    How to frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - This is a function prototype
+ * @head: Recive the value of the main function
+ * Description: Function that frees a listint_t list
+ * section Header: Section description
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
