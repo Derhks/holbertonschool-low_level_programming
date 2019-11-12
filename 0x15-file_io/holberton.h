1e33fd8db8a3d3835758dbfc34f454baf4111081
Derhks <derhks@gmail.com>
2019-11-12 22:58:29 +0000

    [add] including libraries

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
