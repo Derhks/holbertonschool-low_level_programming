bd19722d8b4782722e442507908076002535bb70
Derhks <derhks@gmail.com>
2019-12-15 22:22:42 +0000

    [Add] possible cases when adding the node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,40 +13,43 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int cnt = 0;
-	dlistint_t *tmp;
-	dlistint_t *new_node;
+	unsigned int cnt, cnt1 = 0;
+	dlistint_t *tmp, *tmp1, *new_node;
 
 	if (h == NULL)
-	{
 		return (NULL);
-	}
-
-	new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-
 	tmp = *h;
-
-	while (tmp != NULL)
+	tmp1 = *h;
+	for (cnt = 0; tmp != NULL; cnt++)
+		tmp = tmp->next;
+	if (idx > cnt)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
 	{
-		if (cnt == idx)
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+	while (tmp1 != NULL)
+	{
+		if (cnt1 == idx)
 		{
-			new_node->n = n;
-			new_node->next = tmp->next;
-			tmp->next->prev = new_node;
-			tmp->prev->next = new_node;
-			new_node->prev = tmp->prev;
+			new_node->next = tmp1->next;
+			new_node->prev = tmp1;
+			if (tmp1->next != NULL)
+				tmp1->next->prev = new_node;
+			tmp1->next = new_node;
 			break;
 		}
-		else
-		{
-			cnt++;
-			tmp = tmp->next;
-		}
+		cnt1++;
+		tmp1 = tmp1->next;
 	}
 	return (new_node);
 }
