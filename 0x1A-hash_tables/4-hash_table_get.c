653712c880e00f6d66971599aec69e910ccc3215
Derhks <derhks@gmail.com>
2020-01-19 21:19:53 +0000

    [Fix] If statement

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,17 +14,15 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *tmp = NULL;
 	unsigned long int hash_idx = key_index((unsigned char *)key, ht->size);
 
-	if (ht == NULL || key == NULL || key == '\0')
-		return (NULL);
-	if (ht->array[hash_idx] != NULL)
+	if (ht != NULL)
 	{
+		if (key == NULL || *key == '\0')
+			return (NULL);
 		tmp = ht->array[hash_idx];
 		while (tmp != NULL)
 		{
 			if (strcmp(tmp->key, key) == 0)
-			{
 				return (tmp->value);
-			}
 			tmp = tmp->next;
 		}
 		return (NULL);
