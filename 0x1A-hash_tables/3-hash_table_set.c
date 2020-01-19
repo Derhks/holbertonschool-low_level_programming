ccbe499e9afc44668d032da088328f2565daccdc
Derhks <derhks@gmail.com>
2020-01-19 16:13:32 +0000

    [Fix] change in line 43

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,7 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			{
 				free(tmp->value);
 				tmp->value = new_node_beg->value;
-				//free(new_node_beg->key);
+				free(new_node_beg->key);
 				free(new_node_beg->value);
 				free(new_node_beg);
 				break;
