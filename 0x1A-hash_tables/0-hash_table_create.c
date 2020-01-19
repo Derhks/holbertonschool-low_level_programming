3d64522c4c605529a3425a595f64d5bd5031fce0
Derhks <derhks@gmail.com>
2020-01-19 18:39:36 +0000

    [Delete] statement if unnecessary

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,10 +13,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_hash_table = NULL;
 	unsigned long int itr = 0;
 
-	if (size == 0)
-		return (NULL);
 	new_hash_table = malloc(sizeof(hash_table_t));
-	if (new_hash_table == NULL)
+	if (new_hash_table == NULL || size == 0)
 		return (NULL);
 	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_hash_table->array == NULL)
