395f44a379be1eb4239a6f8cb55a6d473de8f06d
Derhks <derhks@gmail.com>
2020-01-19 17:49:32 +0000

    [Fix] casting variable key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,7 +11,6 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash = hash_djb2(key);
-
+	unsigned long int hash = hash_djb2((unsigned char *)key);
 	return (hash % size);
 }
