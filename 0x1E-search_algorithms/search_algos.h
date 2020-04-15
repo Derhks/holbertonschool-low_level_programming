2f66d7918869c395692a38ec4b1547df26bf7815
Derhks <derhks@gmail.com>
2020-04-15 22:26:54 +0000

    [Create] a header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_SEARCH_ALGOS_H
+#define HEADER_SEARCH_ALGOS_H
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
